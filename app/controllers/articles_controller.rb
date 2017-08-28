da4ffecaa4431ccc58d4470650666d0f1975f56c
JunePaloma <kborkoski@gmail.com>
2017-08-28 06:32:26 -0600

    Add changes to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+before_action :set_article, only: [:destroy, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -15,14 +15,36 @@ class ArticlesController < ApplicationController
 
   def create
   @article = Article.new(article_params)
-
   @article.save
+  flash.notice = "Article '#{{@article.title}}' created!"
   redirect_to article_path(@article)
   end
 
+def destroy
+  @article.delete
+  flash.notice = "Article '#{{@article.title}}' destroyed!"
+  redirect_to articles_path
+end
+
+def edit
+
+end
+
+def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+  flash.notice = "Article '#{{@article.title}}' Updated!"
+  redirect_to article_path(@article)
+end
 
 private
 def article_params
   params.require(:article).permit(:title, :body)
 end
+
+def set_article
+  @article = Article.find(params[:id])
+end
+
+#does update go down here?
 end
